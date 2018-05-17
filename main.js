72eb0db87c88ca5d13d01659f7aceb3f5d54f232
Chelsea <chelseafk534@gmail.com>
2018-05-17 17:21:01 +0800

    modify JS

---
+++ main.js
@@ -0,0 +1,58 @@
+var pageCounter =1
+var animalContainer=document.getElementById("animal-info");;
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+	var Request = new XMLHttpRequest();
+	Request.open('GET','https://chelsea534.github.io/testRepo/mainFunction/test'+pageCounter+'.json' );
+	Request.onload=function(){
+	if(Request.status>=200 && Request.status<400){
+		var ourData=JSON.parse(Request.responseText);
+		renderHTML(ourData);
+	}
+	else{
+		console.log("Error is returned");
+	}
+};
+
+Request.onerror = function(){
+	console.log("Connection error");
+};
+
+Request.send();
+pageCounter++;
+if(pageCounter>3){
+	btn.classList.add("hide-me");
+}
+})	;
+
+function renderHTML(data){
+var htmlString ="";
+for(i=0; i<data.length; i++){
+	htmlString+="<p>"+data[i].name+" is a "+data[i].species+" that likes to eat ";
+
+	for(x=0;x<data[i].foods.likes.length; x++){
+		if(x==0){
+			htmlString+=data[i].foods.likes[x];
+		}
+		else{
+			htmlString+=" and "+data[i].foods.likes[x];
+		}
+	}
+	htmlString+=' and dislikes ';
+
+	for(x=0;x<data[i].foods.dislikes.length; x++){
+		if(x==0){
+			htmlString+=data[i].foods.dislikes[x];
+		}
+		else{
+			htmlString+=" and "+data[i].foods.dislikes[x];
+		}
+	}
+
+	htmlString+='.</p>';
+}
+
+animalContainer.insertAdjacentHTML('beforeend', htmlString);	
+
+}
